74ae8c6b32326d476c6c54666b49c94d9d0905b5
Nikita <zakhrek@debian-pc.pc.ru>
2026-02-20 23:07:49 +0500

    finaly test version

--- client/main.py
+++ client/main.py
@@ -1,4 +1,6 @@
 import requests
+import json
 
-res = requests.get("http://127.0.0.1:5000/api/main")
+res = requests.put("http://127.0.0.1:5000/api/video/2", json={"name": "C", "number": 36})
+
 print(res.json())
--- server/main.py
+++ server/main.py
@@ -1,16 +1,41 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api()
 
+video = {
+    1: {"name": "Python", "number": 16}, 
+    2: {"name": "PHP", "number": 21}
+}
+
 class Main(Resource):
-    def get(self):
-        return {"info": "Test info", "num": 200}
+    def get(self, video_id):
+        if video_id == 0:
+            return video
+        else:
+            return video[video_id]
     
+    def delete(self, video_id):
+        del video[video_id]
+        return video
+    
+    def post(self, video_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument("name", type=str)
+        parser.add_argument("number", type=int)
+        video[video_id] = parser.parse_args()
+        return video
+    
+    def put(self, video_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument("name", type=str)
+        parser.add_argument("number", type=int)
+        video[video_id] = parser.parse_args()
+        return video
 
 
-api.add_resource(Main, "/api/main")
+api.add_resource(Main, "/api/video/<int:video_id>")
 api.init_app(app)
 
 if __name__ == "__main__":
