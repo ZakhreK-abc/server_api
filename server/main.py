7abf3dbfbff3b6c05eab1c4c62fe6d57478a5405
Nikita <zakhrek@debian-pc.pc.ru>
2026-02-20 22:22:17 +0500

    Revert "add venv directory."
    This reverts commit 0b0279255069222dd510b01d7fdef05cebfaf536.

--- server/main.py
+++ server/main.py
@@ -14,4 +14,4 @@ api.add_resource(Main, "/api/main")
 api.init_app(app)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000, host="127.0.0.1")
+    app.run(debug=True, port=5000, host="127.0.0.1")
